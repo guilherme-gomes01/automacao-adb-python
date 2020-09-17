ad2d16a0f632a6f588e1742e19729de758ed1797
guilherme-gomes01 <guilhermejgsg01@gmail.com>
2020-09-16 21:19:32 -0400

    Add device_functions file

---
+++ script_test_device_functions.py
@@ -0,0 +1,15 @@
+import subprocess
+
+def get_devices():
+    output = subprocess.Popen('adb devices', shell=True, stdout=subprocess.PIPE).communicate()[0]
+    output = str(output).split('attached')[1]
+    output = output.split('device')[0]
+    output = output.replace("\\n", "").replace("\\r", "")
+    output = output.replace("\\t", "")
+    return output
+
+def get_screen(serial):
+    output = subprocess.Popen('adb -s %s shell uiautomator dump'%serial,shell=True,stdout=subprocess.PIPE).communicate()[0]
+    print(output)
+    output = subprocess.Popen('adb -s %s pull /sdcard/window_dump.xml'%serial,shell=True,stdout=subprocess.PIPE).communicate()[0]
+    print(output)
