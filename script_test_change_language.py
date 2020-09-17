e265c6eb80a9ccaa77475b75313618a24c78c739
guilherme-gomes01 <guilhermejgsg01@gmail.com>
2020-09-16 21:41:41 -0400

    Update files

--- script_test_change_language.py
+++ script_test_change_language.py
@@ -1,34 +1,36 @@
 import subprocess
-#import xml.etree.ElementTree as ET
 import time
-import script_test_search as teste1
+import script_test_device_functions as dev
+import script_test_events as events
+import script_test_screen_functions as screen
+import script_test_object_functions as obj
 
-device = teste1.get_devices()
-teste1.record_screen()
+device = dev.get_devices()
+screen.record_screen()
 
 #pegar tela e abrir configurações
-teste1.swipe_screen()
-teste1.get_screen(device)
-teste1.get_apps('Configurar')
+events.swipe_screen()
+dev.get_screen(device)
+obj.get_objects('Configurar')
 
 #pegar tela e abrir sistema
 time.sleep(2)
-teste1.swipe_screen()
-teste1.get_screen(device)
-teste1.get_apps('Sistema')
+events.swipe_screen()
+dev.get_screen(device)
+obj.get_objects('Sistema')
 
 #pegar tela e abrir idiomas e entrada
 time.sleep(2)
-teste1.get_screen(device)
-teste1.get_apps('Idiomas e entrada')
+dev.get_screen(device)
+obj.get_objects('Idiomas e entrada')
 
 #pegar tela e alterar idioma
 time.sleep(2)
-teste1.get_screen(device)
-teste1.get_apps('Idiomas')
+dev.get_screen(device)
+obj.get_objects('Idiomas')
 
 time.sleep(2)
-teste1.get_screen(device)
+dev.get_screen(device)
 
 output = subprocess.Popen("adb shell input swipe 926 242 926 500 500", shell=True, stdout=subprocess.PIPE)
 time.sleep(2)
--- script_test_events.py
+++ script_test_events.py
@@ -14,3 +14,7 @@ def event_back():
 def tap_screen(app_name, x, y):
     print("Abrindo %s" % app_name)
     output = subprocess.Popen("adb shell input tap %s %s" % (x, y), shell=True, stdout=subprocess.PIPE)
+
+def swipe_screen():
+    output = subprocess.Popen("adb shell input swipe 300 2000 300 500 500", shell=True, stdout=subprocess.PIPE)
+    time.sleep(2)
