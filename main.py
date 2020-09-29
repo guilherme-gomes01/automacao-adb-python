f59c3ba39c224969a7c8d8cbf5afa75e669d1f27
guilherme-gomes01 <guilhermejgsg01@gmail.com>
2020-09-28 21:02:36 -0400

    Rename files

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-import script_test_open_app_v2 as teste
-teste.record_screen()
+import script_test_open_app_v1 as openApp
+import script_test_search as search
+import script_test_change_language as cLanguage
+import script_test_transfer_files as tFiles
+import script_test_install_uninstall as installUnistall
+import script_test_inverting_colors as colors
+import script_test_screen_rotation as sRotation
+
+
+print ("Essa merda não funciona, desgraaaaçaaaa")
